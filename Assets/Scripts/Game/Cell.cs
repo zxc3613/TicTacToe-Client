f46b67b80f25ca61c9856db91a9e79beebd23e25
OhIlKang <zxc3613@naver.com>
2019-12-24 10:08:12 +0900

    어제 한 내용 셀 만듬

---
+++ Assets/Scripts/Game/Cell.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MarkerType {None, Circle, Cross}
+public class Cell : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer MarkerSpriteRenderer;
+
+    [SerializeField] Sprite circleMarkerSprite;
+    [SerializeField] Sprite crossMarkerSprite;
+
+    public int index;
+
+    MarkerType markerType;
+    public MarkerType MarkerType
+    {
+        get { return markerType; }
+        set 
+        {
+            switch (value)
+            {
+                case MarkerType.None:
+                    MarkerSpriteRenderer.sprite = null;
+                    break;
+                case MarkerType.Circle:
+                    MarkerSpriteRenderer.sprite = circleMarkerSprite;
+                    break;
+                case MarkerType.Cross:
+                    MarkerSpriteRenderer.sprite = crossMarkerSprite;
+                    break;
+            }
+            markerType = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/TicTacToeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicTacToeManager : MonoBehaviour
+{
+    [SerializeField] Cell cell;
+
+    void Start()
+    {
+        cell.MarkerType = MarkerType.Circle;
+    }
+
+    void Update()
+    {
+        
+    }
+}
